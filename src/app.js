98e675a5d91cc0263a52be3cfa813cffa8a8f5cd
KashyapGodhani <patelkashyap599@gmail.com>
2024-06-22 19:41:31 +0530

    add boiler plate code of api

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dbConnection = require("./config/db");
+dbConnection.connection();
+const apiRoute = require("./routes/index");
+const errorHandler = require("./middlewares/error.handler");
+const cors = require("cors");
+const helmet = require('helmet')
+
+
+const app = express();
+app.use(helmet());  // make api header secure
+// const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    // origin: allowedOrigins,
+    origin: "*",
+  })
+);
+  
+// API routes
+app.use("/api/v1/", apiRoute);
+
+app.use(errorHandler);
+
+module.exports = app;
